1e4ffa407f2a48ec73354627f516b07836bcef30
Gisselle Garcia <gissellegar46@gmail.com>
2023-03-23 11:37:27 -0700

    Add border to form for easier viewing

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,21 +1,22 @@
 import { useState } from "react";
 import {useEffect}  from  "react";
 
+
 const Form = (props) => {
 
-  //QUESTION: WHY ARE WE DOING IT LIKE THIS???
   const {initialAnimal = {
                           id_animal: null, 
                           nickname: "", 
                           animal_record_timestamp: "",
                           date_of_sighting: "",
                           location_of_sighting: "",
-                          healthy: ""}} = props;
+                          }} = props;
 
 
   // This is the original State with not initial student 
   const [animal, setAnimal] = useState(initialAnimal);
   const [species, setSpecies] = useState([]);
+  const [animals, setAnimals] = useState([]);
 
   //create functions that handle the event of the user typing into the form//---------------------------------------------------------------------------
   const handleNicknameChange = (event) => {
@@ -35,10 +36,10 @@ const Form = (props) => {
     setAnimal((animal) => ({ ...animal, location_of_sighting }));
   }
 
-//health of animal-----------------------------------------------------------------------------------
-  const handleHealth = (event) => {
-    const healthy = event.target.value;
-    setAnimal((animal) => ({ ...animal, healthy}));
+//email-----------------------------------------------------------------------------------
+  const handleEmail = (event) => {
+    const email = event.target.value;
+    setAnimal((animal) => ({ ...animal, email}));
   }
 
 
@@ -73,6 +74,16 @@ useEffect(() => {
         });
 }, []);
 
+
+//ANIMALS get request setting animals state (A repeat)-----------------------------------------------------------------
+useEffect(() => {
+  fetch("http://localhost:8085/api/animals")
+    .then((response) => response.json())
+    .then((animals) => {
+          setAnimals(animals);
+        });
+}, []);
+
 //A function to handle the Update request//-----------------------------------------------------------------
     const updateAnimal = (existingAnimal) =>{
       return fetch(`http://localhost:8085/api/animals/${existingAnimal.id}`, {
@@ -99,70 +110,116 @@ useEffect(() => {
     
   };
 //--------------------------------------------------------------------------------------------------------
-  return (
-    <div>
-    <form onSubmit={handleSubmit}>
-      <fieldset>
+//   return (
+//     <div>
+//     <form onSubmit={handleSubmit}>
+//       <fieldset>
 
-        <label>Species</label>
-        <select>
-        {species.map((element) => {
-            return (
-              <option value={element.id_species}>
-                {element.species_name}{" "}
-              </option>
-            );
-          })}
-          <option value={-1}>create a new animal</option>
+//         <label>Species</label>
+//         <select>
+//         {species.map((element) => {
+//             return (
+//               <option value={element.id_species}>
+//                 {element.species_name}{" "}
+//               </option>
+//             );
+//           })}
+//           <option value={-1}>create a new animal</option>
 
-        </select>
+//         </select>
         
       
-        <label>Animal Name</label>
-        <input
-          type="text"
-          id="add-animal-name"
-          placeholder="Animal Name"
-          required
-          value={animal.nickname}
-          onChange={handleNicknameChange}
-        />
+//         <label>Animal Name</label>
+//         <input
+//           type="text"
+//           id="add-animal-name"
+//           placeholder="Animal Name"
+//           required
+//           value={animal.nickname}
+//           onChange={handleNicknameChange}
+//         />
 
-        <label>Date of Sighting</label>
-        <input
-          type="date"
-          id="add-sighting-date"
-          placeholder="Sighting Date"
-          required
-          value={animal.date_of_sighting}
-          onChange={handleDateOfSightingChange}
-        />
+//         <label>Date of Sighting</label>
+//         <input
+//           type="date"
+//           id="add-sighting-date"
+//           placeholder="Sighting Date"
+//           required
+//           value={animal.date_of_sighting}
+//           onChange={handleDateOfSightingChange}
+//         />
 
-        <label>Location of Sighting</label>
-        <input
-          type="text"
-          id="add-location-text"
-          placeholder="Animal Location"
-          required
-          value={animal.location_of_sighting}
-          onChange={handleLocationOfSightingChange}
-        />
+//         <label>Location of Sighting</label>
+//         <input
+//           type="text"
+//           id="add-location-text"
+//           placeholder="Animal Location"
+//           required
+//           value={animal.location_of_sighting}
+//           onChange={handleLocationOfSightingChange}
+//         />
 
-        <label>Health</label>
-        <input
-          type="text"
-          id="add-health"
-          placeholder="Animal Health"
-          required
-          value={animal.healthy}
-          onChange={handleHealth}
-        />
 
-      </fieldset>
-      <button type="submit">{!animal.id_animal ? "ADD": "SAVE"}</button>
-    </form>
-    </div>
-  );
+//       </fieldset>
+//       <button type="submit">{!animal.id_animal ? "ADD": "SAVE"}</button>
+//     </form>
+//     </div>
+//   );
+// };
+
+return (
+  <div>
+  <form onSubmit={handleSubmit}>
+    <fieldset>
+    
+      <label>Animal Name</label>
+         <select>
+         {animals.map((element) => {
+             return (
+               <option value={element.id_animal}>
+                 {element.nickname}{" "}
+               </option>
+             );
+           })}
+           <option value={-1}>create a new animal</option>
+         </select>
+
+      <label>Date of Sighting</label>
+      <input
+        type="date"
+        id="add-sighting-date"
+        placeholder="Sighting Date"
+        required
+        value={animal.date_of_sighting}
+        onChange={handleDateOfSightingChange}
+      />
+
+      <label>Location of Sighting</label>
+      <input
+        type="text"
+        id="add-location-text"
+        placeholder="Animal Location"
+        required
+        value={animal.location_of_sighting}
+        onChange={handleLocationOfSightingChange}
+      />
+
+      <label>Your Email</label>
+      <input
+        type="email"
+        id="add-email"
+        placeholder="Sighter Email"
+        required
+        value={animal.sighter_email}
+        onChange={handleEmail}
+      />
+
+
+    </fieldset>
+    <button type="submit">{!animal.id_animal ? "ADD": "SAVE"}</button>
+  </form>
+  </div>
+);
 };
 
 export default Form;
